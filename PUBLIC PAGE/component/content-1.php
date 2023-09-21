677fee24037105d3ae333db11bc5cf6d54b47ba9
Duonganhdu2002 <124427548+Duonganhdu2002@users.noreply.github.com>
2023-09-21 08:58:06 +0700

    Add files via upload

---
+++ PUBLIC PAGE/component/content-1.php
@@ -0,0 +1,25 @@
+<link rel="stylesheet" href="css/content-1.css">
+<div class="content-1">
+    <div class="content-1-child">
+        <div class="content-1-child-left-side">
+            <div>
+                Modern Interior <br>
+                Design Studio
+            </div>
+            <div>
+                Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet <br> velit. Aliquam vulputate velit imperdiet dolor tempor tristique.
+            </div>
+            <div>
+                <div>
+                    <button class="shop-now">Shop now</button>
+                </div>
+                <div>
+                    <button class="exp">Explore</button>
+                </div>
+            </div>
+        </div>
+        <div class="content-1-child-right-side">
+            <div></div>
+        </div>
+    </div>
+</div>
---
+++ PUBLIC PAGE/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/index.css">
+</head>
+
+<body>
+    <?php
+        include "component/header.php";
+    ?>
+    <?php
+        include "component/content-1.php";
+    ?>
+</body>
+
+</html>
