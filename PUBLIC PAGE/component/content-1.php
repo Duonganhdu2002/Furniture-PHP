0b3adde133548eb07207714255bd24d370516412
Duonganhdu2002 <124427548+Duonganhdu2002@users.noreply.github.com>
2023-09-21 09:18:54 +0700

    Add files via upload

--- PUBLIC PAGE/component/content-1.php
+++ PUBLIC PAGE/component/content-1.php
@@ -1,24 +1,24 @@
 <link rel="stylesheet" href="css/content-1.css">
 <div class="content-1">
-    <div class="content-1-child">
-        <div class="content-1-child-left-side">
-            <div>
+    <div class="child">
+        <div class="left-side">
+            <div class="div-1">
                 Modern Interior <br>
                 Design Studio
             </div>
-            <div>
+            <div class="div-2">
                 Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet <br> velit. Aliquam vulputate velit imperdiet dolor tempor tristique.
             </div>
-            <div>
+            <div class="div-3">
                 <div>
                     <button class="shop-now">Shop now</button>
                 </div>
                 <div>
-                    <button class="exp">Explore</button>
+                    <button class="explore">Explore</button>
                 </div>
             </div>
         </div>
-        <div class="content-1-child-right-side">
+        <div class="right-side">
             <div></div>
         </div>
     </div>
---
+++ PUBLIC PAGE/views/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/index.css">
+    <style>
+        body {
+            margin: 0;
+            font-family: "Inter", sans-serif;
+        }
+    </style>
+</head>
+
+<body>
+    <?php
+    include "component/header.php";
+    ?>
+    <?php
+    include "component/content-1.php";
+    ?>
+</body>
+
+</html>
