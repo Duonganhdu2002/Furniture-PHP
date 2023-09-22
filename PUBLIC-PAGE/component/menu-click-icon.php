edd6b8b998ae75bd4f429607f224160ab50dcea5
Wydanhdu <Wydanhdu@gmail.com>
2023-09-22 12:41:53 +0700

    nsjhjs

--- PUBLIC-PAGE/component/menu-click-icon.php
+++ PUBLIC-PAGE/component/menu-click-icon.php
@@ -1,21 +1,50 @@
 <link rel="stylesheet" href="/PUBLIC-PAGE/css/menu-click-icon.css">
-<div class="menu-click-icon">
+<script>
+    document.addEventListener('DOMContentLoaded', function () {
+        const menuIcon = document.querySelector('.menu-icon');
+        const menu = document.querySelector('.menu-click-icon');
+
+        function toggleMenu() {
+            if (menu.style.display === 'none' || menu.style.display === '') {
+                menu.style.display = 'contents';
+            } else {
+                menu.style.display = 'none';
+            }
+        }
+
+        menuIcon.addEventListener('click', toggleMenu);
+
+        function hideMenuIfLargeScreen() {
+            if (window.innerWidth > 768) {
+                menu.style.display = 'none';
+            }
+        }
+
+        hideMenuIfLargeScreen();
+
+        window.addEventListener('resize', hideMenuIfLargeScreen);
+    });
+</script>
+
+<div class="menu-click-icon" style="display: none;">
     <div>
-        <a class="menu-link" href="">Home</a>
-    </div>
-    <div>
-        <a class="menu-link" href="">Shop</a>
-    </div>
-    <div>
-        <a class="menu-link" href="">About us</a>
-    </div>
-    <div>
-        <a class="menu-link" href="">Services</a>
-    </div>
-    <div>
-        <a class="menu-link" href="">Blog</a>
-    </div>
-    <div>
-        <a class="menu-link" href="">Contact us</a>
+        <div>
+            <a class="menu-link" href="">Home</a>
+        </div>
+        <div>
+            <a class="menu-link" href="">Shop</a>
+        </div>
+        <div>
+            <a class="menu-link" href="">About us</a>
+        </div>
+        <div>
+            <a class="menu-link" href="">Services</a>
+        </div>
+        <div>
+            <a class="menu-link" href="">Blog</a>
+        </div>
+        <div>
+            <a class="menu-link" href="">Contact us</a>
+        </div>
     </div>
 </div>
