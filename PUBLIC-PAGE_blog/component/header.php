8185e742e251a290f3bbb2152ad28c01262bdc04
Nguyễn Văn Phú Quí <127293725+PhuuQuiii@users.noreply.github.com>
2023-09-23 22:19:28 +0700

    Add files via upload

---
+++ PUBLIC-PAGE_blog/component/header.php
@@ -0,0 +1,35 @@
+<link rel="stylesheet" href="css/header.css">
+<div class="header">
+    <div class="header-child">
+        <div class="logo-brand">
+            <a href="">Brand<span>.</span></a>
+        </div>
+        <div class="menu">
+            <div>
+                <a class="menu-link" href="">Home</a>
+            </div>
+            <div><a class="menu-link" href="">Shop</a></div>
+            <div><a class="menu-link" href="">About us</a></div>
+            <div><a class="menu-link" href="">Services</a></div>
+            <div><a class="menu-link" href="">Blog</a></div>
+            <div><a class="menu-link" href="">Contact us</a></div>
+        </div>
+        <div class="user-cart-icon">
+            <div class="user-icon">
+                <a href="#">
+                    <img src="/PUBLIC-PAGE/images/user.svg" alt="ădawdaw">
+                </a>
+            </div>
+            <div class="cart-icon">
+                <a href="#">
+                    <img src="/PUBLIC-PAGE/images/cart.svg" alt="">
+                </a>
+            </div>
+        </div>
+        <div class="menu-icon">
+            <a href="#">
+                <img src="/PUBLIC-PAGE/images/menu.svg" alt="">
+            </a>
+        </div>
+    </div>
+</div>
