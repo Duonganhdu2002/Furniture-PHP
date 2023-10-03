499ce9f992b9ceadb9f6fe5cc43d0a6f2bffc4dd
Wydanhdu <Wydanhdu@gmail.com>
2023-10-03 18:03:56 +0700

    ?

--- PUBLIC-PAGE/component/content-6.php
+++ PUBLIC-PAGE/component/content-6.php
@@ -1,9 +1,26 @@
 <link rel="stylesheet" href="css/content-6.css">
-<div class="footer">
-    <div class="container-ct6">
-        <div class="container-ct6-first-child">
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+
+<!-- Start Footer Section -->
+<footer class="footer-section">
+    <div class="relative">
+
+        <div class="sofa-img">
+            <img src="images/sofa.png" alt="Image" class="img-fluid">
+        </div>
+
+        <div class="col-lg-8">
             <div class="subscription-form">
-                <h3 class="d-flex align-items-center"><span class="me-1"><img src="images/envelope-outline.svg" alt="Image" class="img-fluid"></span><span>Subscribe to Newsletter</span></h3>
+
+                <h3 class="d-flex align-items-center">
+                    <span class="me-1">
+                        <img src="images/envelope-outline.svg" alt="Image" class="img-fluid">
+                    </span>
+                    <span style="margin-left: 10px">
+                        Subscribe to Newsletter
+                    </span>
+                </h3>
+
                 <form action="#" class="row g-3">
                     <div class="col-auto">
                         <input type="text" class="form-control" placeholder="Enter your name">
@@ -17,17 +34,84 @@
                         </button>
                     </div>
                 </form>
-            </div>
-            <div class="sofa-img">
-                <img src="images/sofa.png" alt="Image" class="img-fluid">
+
             </div>
         </div>
-        <div class="container-ct6-second-child">
-            div-2
+
+        <div class="g-5 mb-5">
+            <div class="col-lg-4">
+                <div class="mb-4 footer-logo-wrap"><a href="#" class="footer-logo">Furni<span>.</span></a></div>
+                <p class="mb-4">Donec facilisis quam ut purus rutrum lobortis. Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam vulputate velit imperdiet dolor tempor tristique. Pellentesque habitant</p>
+
+                <ul class="list-unstyled custom-social">
+                    <li><a href="#"><span class="fa fa-brands fa-facebook-f"></span></a></li>
+                    <li><a href="#"><span class="fa fa-brands fa-twitter"></span></a></li>
+                    <li><a href="#"><span class="fa fa-brands fa-instagram"></span></a></li>
+                    <li><a href="#"><span class="fa fa-brands fa-linkedin"></span></a></li>
+                </ul>
+            </div>
+
+            <div class="col-lg-8">
+                <div class="row links-wrap">
+                    <div class="col-6 col-sm-6 col-md-3">
+                        <ul class="list-unstyled">
+                            <li><a href="#">About us</a></li>
+                            <li><a href="#">Services</a></li>
+                            <li><a href="#">Blog</a></li>
+                            <li><a href="#">Contact us</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="col-6 col-sm-6 col-md-3">
+                        <ul class="list-unstyled">
+                            <li><a href="#">Support</a></li>
+                            <li><a href="#">Knowledge base</a></li>
+                            <li><a href="#">Live chat</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="col-6 col-sm-6 col-md-3">
+                        <ul class="list-unstyled">
+                            <li><a href="#">Jobs</a></li>
+                            <li><a href="#">Our team</a></li>
+                            <li><a href="#">Leadership</a></li>
+                            <li><a href="#">Privacy Policy</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="col-6 col-sm-6 col-md-3">
+                        <ul class="list-unstyled">
+                            <li><a href="#">Nordic Chair</a></li>
+                            <li><a href="#">Kruzo Aero</a></li>
+                            <li><a href="#">Ergonomic Chair</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
         </div>
+
         <hr>
-        <div class="container-ct6-third-child">
-            div-3
+
+        <div class="border-top copyright">
+            <div class="pt-4">
+                <div class="col-lg-6">
+                    <p class="mb-2 text-center text-lg-start">Copyright &copy;<script>
+                            document.write(new Date().getFullYear());
+                        </script>. All Rights Reserved. &mdash; Designed with love by <a href="https://untree.co">Untree.co</a> <!-- License information: https://untree.co/license/ -->
+                    </p>
+                </div>
+
+                <div class="col-lg-6 text-center text-lg-end">
+                    <ul class="list-unstyled d-inline-flex ms-auto">
+                        <li class="me-4"><a href="#">Terms &amp; Conditions</a></li>
+                        <li><a href="#">Privacy Policy</a></li>
+                    </ul>
+                </div>
+
+            </div>
         </div>
+
     </div>
-</div>
+</footer>
+<!-- End Footer Section -->
