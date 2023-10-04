294fe33aa7463a0d9117f8c995e54511757171f6
Wydanhdu <Wydanhdu@gmail.com>
2023-10-04 13:02:55 +0700

    hihi

--- PUBLIC-PAGE/component/content-6.php
+++ PUBLIC-PAGE/component/content-6.php
@@ -3,7 +3,7 @@
 
 <!-- Start Footer Section -->
 <footer class="footer-section">
-    <div class="relative">
+    <div style="margin-top: 0px;" class="relative">
 
         <div class="sofa-img">
             <img src="images/sofa.png" alt="Image" class="img-fluid">
@@ -21,14 +21,14 @@
                     </span>
                 </h3>
 
-                <form action="#" class="row g-3">
-                    <div class="col-auto">
+                <form action="#" class="input-form">
+                    <div>
                         <input type="text" class="form-control" placeholder="Enter your name">
                     </div>
-                    <div class="col-auto">
+                    <div>
                         <input type="email" class="form-control" placeholder="Enter your email">
                     </div>
-                    <div class="col-auto">
+                    <div>
                         <button class="btn btn-primary">
                             <span class="fa fa-paper-plane"></span>
                         </button>
@@ -38,12 +38,12 @@
             </div>
         </div>
 
-        <div class="g-5 mb-5">
-            <div class="col-lg-4">
-                <div class="mb-4 footer-logo-wrap"><a href="#" class="footer-logo">Furni<span>.</span></a></div>
-                <p class="mb-4">Donec facilisis quam ut purus rutrum lobortis. Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam vulputate velit imperdiet dolor tempor tristique. Pellentesque habitant</p>
+        <div style="display: flex; margin-bottom: 60px">
+            <div style="width: 30%;">
+                <div style="font-weight: bold;" class="mb-4 footer-logo-wrap"><a href="#" class="footer-logo">Furni<span>.</span></a></div>
+                <p style="line-height: 1.8; font-size: 14px; opacity: 0.7" class="mb-4">Donec facilisis quam ut purus rutrum lobortis. Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam vulputate velit imperdiet dolor tempor tristique. Pellentesque habitant</p>
 
-                <ul class="list-unstyled custom-social">
+                <ul style="display: flex; justify-content: start;" class="custom-social">
                     <li><a href="#"><span class="fa fa-brands fa-facebook-f"></span></a></li>
                     <li><a href="#"><span class="fa fa-brands fa-twitter"></span></a></li>
                     <li><a href="#"><span class="fa fa-brands fa-instagram"></span></a></li>
@@ -51,9 +51,9 @@
                 </ul>
             </div>
 
-            <div class="col-lg-8">
-                <div class="row links-wrap">
-                    <div class="col-6 col-sm-6 col-md-3">
+            <div style="width: 70%; margin-top: 25px">
+                <div class="links-wrap">
+                    <div class="text-column">
                         <ul class="list-unstyled">
                             <li><a href="#">About us</a></li>
                             <li><a href="#">Services</a></li>
@@ -62,7 +62,7 @@
                         </ul>
                     </div>
 
-                    <div class="col-6 col-sm-6 col-md-3">
+                    <div class="text-column">
                         <ul class="list-unstyled">
                             <li><a href="#">Support</a></li>
                             <li><a href="#">Knowledge base</a></li>
@@ -70,7 +70,7 @@
                         </ul>
                     </div>
 
-                    <div class="col-6 col-sm-6 col-md-3">
+                    <div class="text-column">
                         <ul class="list-unstyled">
                             <li><a href="#">Jobs</a></li>
                             <li><a href="#">Our team</a></li>
@@ -79,7 +79,7 @@
                         </ul>
                     </div>
 
-                    <div class="col-6 col-sm-6 col-md-3">
+                    <div class="text-column">
                         <ul class="list-unstyled">
                             <li><a href="#">Nordic Chair</a></li>
                             <li><a href="#">Kruzo Aero</a></li>
@@ -91,26 +91,27 @@
 
         </div>
 
-        <hr>
+        <hr style="border: none; border-top: 1px solid rgba(128, 128, 128, 0.3);">
 
-        <div class="border-top copyright">
-            <div class="pt-4">
-                <div class="col-lg-6">
-                    <p class="mb-2 text-center text-lg-start">Copyright &copy;<script>
-                            document.write(new Date().getFullYear());
-                        </script>. All Rights Reserved. &mdash; Designed with love by <a href="https://untree.co">Untree.co</a> <!-- License information: https://untree.co/license/ -->
-                    </p>
-                </div>
 
-                <div class="col-lg-6 text-center text-lg-end">
-                    <ul class="list-unstyled d-inline-flex ms-auto">
-                        <li class="me-4"><a href="#">Terms &amp; Conditions</a></li>
-                        <li><a href="#">Privacy Policy</a></li>
-                    </ul>
-                </div>
-
+        <div style="display: flex; font-size: 14px; opacity: 0.9;">
+            <div style="width: 70%;">
+                <p class="mb-2 text-center text-lg-start">Copyright &copy;
+                    <script>
+                        document.write(new Date().getFullYear());
+                    </script>. All Rights Reserved. &mdash; Designed with love by <a href="https://untree.co">Untree.co</a> <!-- License information: https://untree.co/license/ -->
+                </p>
             </div>
+
+            <div style="width: 30%;">
+                <ul style="display: flex;" class="list-unstyled">
+                    <li style="width: 50%; float: right;" class="me-4"><a href="#">Terms &amp; Conditions</a></li>
+                    <li style="width: 50%;"><a href="#">Privacy Policy</a></li>
+                </ul>
+            </div>
+
         </div>
+
 
     </div>
 </footer>
