422cca51190baaa5d8b32befc0191a6914cf2bb6
duco9t <dung5715@gmail.com>
2023-10-15 04:07:51 +0700

    add logo.svg, fix sofa bi de len header

--- PUBLIC-PAGE/index.php
+++ PUBLIC-PAGE/index.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="icon" type="image/x-icon" href="./images/logo.svg">
     <style>
         body {
             margin: 0;
