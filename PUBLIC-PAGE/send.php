b220c9c22459c8844e91f7140a7daefd597b3b11
PhuuQuiii <nguyenphuqui170304@gmail.com>
2023-10-18 08:33:28 +0700

    sửa đổi send js

--- PUBLIC-PAGE/send.php
+++ PUBLIC-PAGE/send.php
@@ -1,10 +1,14 @@
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+
 <?php
+
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
 require 'phpmailer/src/Exception.php';
 require 'phpmailer/src/PHPMailer.php';
 require 'phpmailer/src/SMTP.php';
+
 
 if(isset($_POST["send"])){
     $mail = new PHPMailer(true);
@@ -25,19 +29,35 @@ if(isset($_POST["send"])){
     $mail->isHTML(true); 
 
     $mail->Subject = 'Phan hoi cua khach hang'; // Chủ đề email
-    $mail->Body = 'First Name: ' . $_POST["firstname"] . '<br>Last Name: ' . $_POST["lastname"] . '<br>Message: ' . $_POST["message"]; // Nội dung email
+    $mail->Body = 'First Name: ' . $_POST["first-name"] . '<br>Last Name: ' . $_POST["last-name"] . '<br>Message: ' . $_POST["message"]; // Nội dung email
 
     try {
         $mail->send();
-        echo
-        "
-        <script>
-        alert('Sent Successfully');
-        window.location.href = 'content-8.php';
-        </script>
+        echo "
+            <script>
+                document.addEventListener('DOMContentLoaded', function () {
+                    Swal.fire({
+                        icon: 'success',
+                        title: 'Sent Successfully',
+                        showConfirmButton: false,
+                        timer: 2500,
+                    }).then(function() {
+                        window.location.href = 'index.php?pid=5';
+                    });
+                });
+            </script>
         ";
     } catch (Exception $e) {
-        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        echo "
+            <script>
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Error!',
+                    text: 'Message could not be sent. Mailer Error: {$mail->ErrorInfo}',
+                });
+            </script>
+        ";
     }
 }
 ?>
+
