48a0a3f2f064f8465bbf256baf54593015bb4233
Wydanhdu <Wydanhdu@gmail.com>
2023-10-26 07:34:27 +0700

    jjiji

--- ADMIN/kt_admin.php
+++ ADMIN/kt_admin.php
@@ -8,27 +8,27 @@ $dbname = "shopping_online";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Lấy tất cả các bản ghi từ cơ sở dữ liệu
-// $sql = "SELECT id, password FROM users WHERE role = 'admin'";
-// $result = $conn->query($sql);
+//Lấy tất cả các bản ghi từ cơ sở dữ liệu
+$sql = "SELECT id, password FROM users WHERE role = 'admin'";
+$result = $conn->query($sql);
 
-// if ($result->num_rows > 0) {
-//     while ($row = $result->fetch_assoc()) {
-//         $userId = $row['id'];
-//         $currentPassword = $row['password'];
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $userId = $row['id'];
+        $currentPassword = $row['password'];
 
-//         // Mã hóa mật khẩu và cập nhật lại vào cơ sở dữ liệu
-//         $password = password_hash($currentPassword, PASSWORD_DEFAULT);
-//         $updateSql = "UPDATE users SET password = '$password' WHERE id = $userId";
-//         if ($conn->query($updateSql) === TRUE) {
-//             echo "Đã cập nhật mật khẩu cho user có ID: " . $userId . "<br>";
-//         } else {
-//             echo "Lỗi khi cập nhật mật khẩu cho user có ID: " . $userId . "<br>";
-//         }
-//     }
-// } else {
-//     echo "Không có user nào trong cơ sở dữ liệu.";
-// }
+        // Mã hóa mật khẩu và cập nhật lại vào cơ sở dữ liệu
+        $password = password_hash($currentPassword, PASSWORD_DEFAULT);
+        $updateSql = "UPDATE users SET password = '$password' WHERE id = $userId";
+        if ($conn->query($updateSql) === TRUE) {
+            echo "Đã cập nhật mật khẩu cho user có ID: " . $userId . "<br>";
+        } else {
+            echo "Lỗi khi cập nhật mật khẩu cho user có ID: " . $userId . "<br>";
+        }
+    }
+} else {
+    echo "Không có user nào trong cơ sở dữ liệu.";
+}
 
 if (isset($_POST['username']) && isset($_POST['password'])) {
     $username = $_POST['username'];
