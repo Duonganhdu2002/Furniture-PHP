f394881ba75e7d9af1831a309bc4130cc3eea43e
duco9t <dung5715@gmail.com>
2023-11-01 23:09:37 +0700

    Sửa layout thông tin sản phẩm

--- PUBLIC-PAGE/component/product-detail.php
+++ PUBLIC-PAGE/component/product-detail.php
@@ -37,24 +37,40 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
             <input type="hidden" name="id" value="<?php echo $id; ?>">
             <h1><?php echo $productName; ?></h1>
             <p>
-               <?php echo $productDescription; ?>
+                <?php echo $productDescription; ?>
             </p>
+            <div style="width:50%; height: 60px; align-items: center; background-color:#ebebeb; display:flex; margin-top: 40px">
+                    <b style="font-size: 45px; margin-left: 20px;"><?php echo $productPrice; ?> $</b>
+            </div>
+            <div style="display: flex; margin-top: 40px; margin-bottom: 40px">
+                
+                <div style="display:flex; align-items: center; width:50%;">
+                <input value="1" type="number" name="quantity" id="quantity" min="1">
+                </div>
+                <div style="display: flex; width:100%; align-items: center; margin-left: 30px">
+                <p>
+                    Available:
+                    <b><?php echo $productQuantity; ?></b>
+                </p>
+                </div>
+            </div>
 
-            <input value="1" type="number" name="quantity" id="quantity" min="1">
-
-            <button>Add to card</button>
-            <button>Buy now</button>
-            <p>
-                <b><?php echo $productPrice; ?></b>
-            </p>
-            <p>
-                Available
-                <b><?php echo $productQuantity; ?></b>
-            </p>
-
-            <a href="index.php?pid=1">
-                <button>Back to shop</button>
-            </a>
+            <div style="display:flex; margin-top: 40px; margin-bottom: 40px; ">
+                <div style="margin-right: 20px;">
+                    <button class="button">
+                    <img src="../PUBLIC-PAGE/images/cart.svg" alt="" style="margin-right:8px">
+                    Add to card
+                    </button>
+                </div>
+                <div>
+                    <button class="button">Buy now</button>
+                </div>
+            </div>
+            <div style="position: relative; height: 150px; /* Chiều cao của phần tử chứa */">
+                <a href="index.php?pid=1" style="position: absolute; bottom: 0; right: 0;">
+                    <button class="button">Back to shop</button>
+                </a>
+            </div>
         </div>
     </div>
 </div>
@@ -71,7 +87,7 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
     }
 
     .product-detail .product-datil-child {
-        width: 68%;
+        width: 80%;
         display: flex;
         height: fit-content;
         padding-top: 50px;
@@ -86,12 +102,50 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
     }
 
     .product-detail .product-datil-child .leftside img {
-        width: 50%;
+        width: 60%;
+        border-radius: 20px;
+        border-bottom: black 10 10 10;
     }
 
     .product-detail .product-datil-child .rightside {
-        width: 50%;
-        padding-left: 40px;
+        width: 60%;
+        padding-left: 100px;
         opacity: 0.8;
+    }
+
+    .product-detail .product-datil-child .rightside h1 {
+        font-size: 40px;
+        display: flex;
+        margin-top: 0px;
+    }
+
+    .product-detail .product-datil-child .rightside input {
+        width:100%;
+        height: 30px;
+        font-size: x-large;
+        text-align: center;
+    }
+
+    .button {
+        display: flex;
+        width: 200px;
+        height: 50px;
+        border-radius: 12px;
+        background-color: black;
+        border: none;
+        color: #fff;
+        cursor:pointer;
+        align-items: center;
+        justify-content: center;
+        font-weight: bold;
+    }
+
+    .button:hover {
+        background-color: #f9bf29;
+        color: black;
+    }
+
+    .button:active {
+        opacity: 0.5;
     }
 </style>
