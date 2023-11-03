480e170d466dfa41047880e600f84bdb9bae472d
Wydanhdu <Wydanhdu@gmail.com>
2023-11-04 00:06:05 +0700

    fix profile

---
+++ PUBLIC-PAGE/component/cart-user.php
@@ -0,0 +1,14 @@
+<div class="user-cart">
+    <div class="user-cart-child">
+        hihi
+    </div>
+</div>
+<style>
+    .user-cart {
+        display: flex;
+        justify-content: center;
+    }
+    .user-cart .user-cart-child {
+        width: 68%;
+    }
+</style>
