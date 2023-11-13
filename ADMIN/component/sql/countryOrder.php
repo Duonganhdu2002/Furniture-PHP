9d1a09ce049963c31d776cbc54372f13e074f0bf
Wydanhdu <Wydanhdu@gmail.com>
2023-11-13 13:41:54 +0700

    Koo

--- ADMIN/component/sql/countryOrder.php
+++ ADMIN/component/sql/countryOrder.php
@@ -8,9 +8,25 @@ $sql = "SELECT address_cart.country, COUNT(*) AS order_count, SUM(shopping_carts
         LIMIT 5";
 
 $result = $conn->query($sql);
-$row = $result->fetch_assoc();
-$totalCountry = isset($row['total_value']) ? $row['total_value'] : 0;
 
+// Check if the query was successful
+if ($result) {
+    // Fetch the first row
+    $row = $result->fetch_assoc();
+
+    // Check if a row was actually fetched
+    if ($row) {
+        $totalCountry = isset($row['total_value']) ? $row['total_value'] : 0;
+    } else {
+        $totalCountry = 0; // Set a default value if no row is fetched
+    }
+} else {
+    // Handle the case where the query fails
+    echo "Error executing query: " . $conn->error;
+    $totalCountry = 0; // Set a default value
+}
+
+// Repeat the process for the second query
 $sql1 = "SELECT address_cart.country, COUNT(*) AS order_count, SUM(shopping_carts.total_price) AS last_total_value
         FROM shopping_carts
         INNER JOIN address_cart ON shopping_carts.id = address_cart.id_cart
@@ -20,11 +36,20 @@ $sql1 = "SELECT address_cart.country, COUNT(*) AS order_count, SUM(shopping_cart
         LIMIT 5";
 
 $result1 = $conn->query($sql1);
-$row1 = $result1->fetch_assoc();
-$totalCountryPreviousYear = $row1['last_total_value'];
-if ($totalCountryPreviousYear === null) {
+
+if ($result1) {
+    $row1 = $result1->fetch_assoc();
+
+    if ($row1) {
+        $totalCountryPreviousYear = $row1['last_total_value'];
+    } else {
         $totalCountryPreviousYear = 0;
+    }
+} else {
+    echo "Error executing query: " . $conn->error;
+    $totalCountryPreviousYear = 0;
 }
 
+// Rest of your code remains the same
 $percentIncreaseCountry = ($totalCountryPreviousYear != 0) ? (($totalCountry / $totalCountryPreviousYear) * 100) : 0;
-$percentIncreaseCountry = round($percentIncreaseCountry,0);
+$percentIncreaseCountry = round($percentIncreaseCountry, 0);
