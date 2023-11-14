60f12e73b08deaa8e39c323914a2adbcb48b47ff
duco9t <dung5715@gmail.com>
2023-11-14 13:11:54 +0700

    admm

--- ADMIN/component/form/member.php
+++ ADMIN/component/form/member.php
@@ -1,3 +1,46 @@
 <div>
-    in process
+    <form action="">
+        <label for="username">Username</label>
+        <input type="text" name="username">
+        <br>
+        <label for="fullname">Full Name</label>
+        <input type="text" name="fullname">
+        <br>
+        <label for="phone">Phone</label>
+        <input type="text" name="phone">
+        <br>
+        <label for="email">Email</label>
+        <input type="text" name="email">
+        <br>
+        <label for="birth">Date of birth</label>
+        <input type="date" name="birth">
+        <br>
+        <label for="username">Gender</label>
+        <select name="gender">
+                    <option value="1">Male</option>
+                    <option value="2">Female</option>
+        </select>
+        <br>
+            <label for="">Country:</label>
+            <input type="text" name="AddressCountry">
+            <br>
+            <label for="">Province:</label>
+            <input type="text" name="AddressProvince">
+            <br>
+            <label for="">District:</label>
+            <input type="text" name="AddressDistrict">
+            <br>
+            <label for="">Commune:</label>
+            <input type="text" name="AddressCommune">
+            <br>
+            <label for="">Street:</label>
+            <input type="text" name="AddressStreet">
+            <br>
+            <label for="">Number:</label>
+            <input type="text" name="AddressNumber">
+            <br>
+        <label for="Avatar">Change Avatar:</label>
+        <input  type="file" id="Avatar" name="Avatar" accept="image/*">
+        <br>
+    </form>
 </div>
