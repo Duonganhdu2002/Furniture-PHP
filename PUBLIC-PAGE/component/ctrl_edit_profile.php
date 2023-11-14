8a59c82e0c2a982e6b0f10cfce77744ae3414c7c
PhuuQuiii <nguyenphuqui170304@gmail.com>
2023-11-14 14:33:20 +0700

    fix add member and edit profile

--- PUBLIC-PAGE/component/ctrl_edit_profile.php
+++ PUBLIC-PAGE/component/ctrl_edit_profile.php
@@ -1,22 +1,22 @@
 <?php
 session_start();
-$username = $_SESSION["username_user"];
 
-// Kết nối đến cơ sở dữ liệu
+// Check if the user is logged in
+if (!isset($_SESSION["username_user"])) {
+    echo "<script>alert('Session không tồn tại.');</script>";
+    exit;
+}
+
+// Database connection
 $conn = new mysqli('localhost', 'root', '', 'shopping_online');
 if ($conn->connect_error) {
     die("Kết nối thất bại: " . $conn->connect_error);
 }
 
-// Kiểm tra sự tồn tại của session
-$username = isset($_SESSION["username_user"]) ? $_SESSION["username_user"] : "";
+// Get the current username from the session
+$username = $_SESSION["username_user"];
 
-if (empty($username)) {
-    echo "<script>alert('Session không tồn tại.');</script>";
-    exit;
-}
-
-// Lấy dữ liệu từ form
+// Get data from the form
 $customerUserName = $_POST["username"];
 $customerName = $_POST["customerName"];
 $customerEmail = $_POST["customerEmail"];
@@ -32,7 +32,7 @@ $customerAddressCountry = $_POST["customerAddressCountry"];
 
 $message = "";
 
-// Kiểm tra dữ liệu từ form
+// Check if any required field is empty
 if (
     empty($customerUserName) || empty($customerName) || empty($customerEmail) || empty($customerPhone) || empty($customerBirth)
     || empty($customerGender)  || empty($customerAddressNumber) || empty($customerAddressStreet) || empty($customerAddressCommune)
@@ -40,7 +40,7 @@ if (
 ) {
     $message = "All the fields are required";
 } else {
-    // Kiểm tra xem `username` mới có sẵn trong cơ sở dữ liệu hay không
+    // Check if the new username already exists
     $checkUsernameQuery = "SELECT * FROM users WHERE username = ?";
     $checkUsernameStmt = $conn->prepare($checkUsernameQuery);
     $checkUsernameStmt->bind_param("s", $customerUserName);
@@ -48,32 +48,33 @@ if (
     $checkUsernameResult = $checkUsernameStmt->get_result();
 
     if ($checkUsernameResult->num_rows > 0) {
-        
-        // Cập nhật thông tin người dùng
+        $message = "Username already exists. Please choose a different one.";
+    } else {
+        // Update user information
         $sqlInformation = "UPDATE information SET username = ?, full_name = ?, date_of_birth = ?, email = ?, gender = ?, phone_number = ? WHERE username = ?";
         $stmtInformation = $conn->prepare($sqlInformation);
         $stmtInformation->bind_param("ssssiss", $customerUserName, $customerName, $customerBirth, $customerEmail, $customerGender, $customerPhone, $username);
         $resultInformation = $stmtInformation->execute();
 
-        // Cập nhật địa chỉ người dùng
+        // Update user address
         $sqlAddresses = "UPDATE addresses SET username = ?, number = ?, street = ?, commune = ?, district = ?, province = ?, country = ? WHERE username = ?";
         $stmtAddresses = $conn->prepare($sqlAddresses);
         $stmtAddresses->bind_param("ssssssss", $customerUserName, $customerAddressNumber, $customerAddressStreet, $customerAddressCommune, $customerAddressDistrict, $customerAddressProvince, $customerAddressCountry, $username);
         $resultAddresses = $stmtAddresses->execute();
 
-        // Cập nhật username trong bảng users
+        // Update username in the users table
         $sqlUsers = "UPDATE users SET username = ? WHERE username = ?";
         $stmtUsers = $conn->prepare($sqlUsers);
         $stmtUsers->bind_param("ss", $customerUserName, $username);
         $resultUsers = $stmtUsers->execute();
 
         if (!$resultInformation || !$resultAddresses || !$resultUsers) {
-            $message = "Invalid query: " . $stmtInformation->error . $stmtAddresses->error . $stmtUsers->error;
+            $message = "Error updating user: " . $stmtInformation->error . $stmtAddresses->error . $stmtUsers->error;
         } else {
-            // Cập nhật session với thông tin mới
+            // Update session with new information
             $_SESSION["username_user"] = $customerUserName;
-            // Các thông tin khác cần cập nhật trong session cũng có thể thêm vào đây
-    
+            // Additional information to update in the session can be added here
+
             $message = "Customer updated correctly";
             header("location: ../index.php?pid=11");
             exit;
@@ -81,6 +82,6 @@ if (
     }
 }
 
-// Hiển thị thông báo
+// Display the message
 echo "<script>alert('$message');</script>";
 ?>
