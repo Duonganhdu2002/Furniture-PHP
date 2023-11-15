87147f6b0d9e37d34cf2e7c16832acc25b59bf3a
Wydanhdu <Wydanhdu@gmail.com>
2023-11-15 09:31:47 +0700

    koko

--- PUBLIC-PAGE/component/ctrl-cart/addToCart.php
+++ PUBLIC-PAGE/component/ctrl-cart/addToCart.php
@@ -1,25 +1,33 @@
 <?php
-
 session_start();
 
 if (isset($_POST["submit"])) {
-    $image = $_POST["image"];
-    $product_name = $_POST["product_name"];
-    $price = $_POST["price"];
     $id = $_POST["id"];
-    $quantity = $_POST["quantity"];
-    if (!isset($quantity)) {
-        $quantity = 1;
-    }
-
-    $productArray = array($image, $product_name, $price, $id, $quantity);
+    $quantity = isset($_POST["quantity"]) ? $_POST["quantity"] : 1;
 
     if (!isset($_SESSION["cart"])) {
         $_SESSION["cart"] = array();
     }
 
-    $_SESSION["cart"][] = $productArray;
+    $productIndex = -1;
+    foreach ($_SESSION["cart"] as $key => $product) {
+        if ($product[3] == $id) {
+            $productIndex = $key;
+            break;
+        }
+    }
 
-    header("Location: ../../index.php?pid=6");
+    if ($productIndex !== -1) {
+        $_SESSION["cart"][$productIndex][4] += $quantity;
+    } else {
+        $image = $_POST["image"];
+        $product_name = $_POST["product_name"];
+        $price = $_POST["price"];
+        $productArray = array($image, $product_name, $price, $id, $quantity);
+        $_SESSION["cart"][] = $productArray;
+    }
+
+    header("Location: " . $_SERVER['HTTP_REFERER']);
     exit();
 }
+?>
